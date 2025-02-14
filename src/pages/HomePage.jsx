3c4cc8fd647e427c4552a3c4b117f64f1ce97851
lynnliang34 <lynnliang34@gmail.com>
2025-02-14 19:15:17 +0800

    feat: 首頁版面

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,14 @@
 export default function HomePage() {
-  return <h1>首頁</h1>;
+  return (
+    <div className="container text-center">
+      <img
+        className="mt-5"
+        src="../src/assets/baby word.png"
+        style={{ width: "400px" }}
+        alt=""
+      />
+      <h1 className="mt-1">寶寶用品店</h1>
+      <img src="../src/assets/infant.png" style={{ width: "400px" }} alt="" />
+    </div>
+  );
 }

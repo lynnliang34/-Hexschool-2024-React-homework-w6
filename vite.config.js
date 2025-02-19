8421f84f2af22f7d9d9b56dc393157c7fc519c15
lynnliang34 <lynnliang34@gmail.com>
2025-02-19 23:04:58 +0800

    feat: 調整 vite.config.js 的 base 路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "./" : "/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/-Hexschool-2024-React-homework-w6/"
+      : "/",
   plugins: [react()],
 });
